f9ba56144a91e83ba3d422707e3b7cec1d926e10
qinshiyang <2242256621@qq.com>
2019-04-10 20:45:25 +0800

    update

--- Falsk-test/leaning-blogs/app.py
+++ Falsk-test/leaning-blogs/app.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import required
+
+
+class NameForm(Form):
+    name = StringField('Whit is your name?', validators=[required()])
+    submit = SubmitField('Submit')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Qinsy'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
