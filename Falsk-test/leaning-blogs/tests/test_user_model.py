5618fb9501e518fbc47c647a0aa86bf55e84d75f
qinshiyang <2242256621@qq.com>
2019-05-05 14:13:18 +0800

    update tests

--- Falsk-test/leaning-blogs/tests/test_user_model.py
+++ Falsk-test/leaning-blogs/tests/test_user_model.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -22,3 +22,13 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
