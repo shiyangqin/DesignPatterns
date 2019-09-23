149dc51f5ae472e36ac82d087a41a8d5450dccc4
shiyangqin <shiyangqin@yeah.net>
2019-09-23 17:53:53 +0800

    修改日志配置位置

--- flask/run.py
+++ flask/run.py
@@ -5,10 +5,11 @@ import logging_config
 from app.app_test import app as app_test
 from config import Log
 
+logging_config.config_logging(Log.log_name, Log.log_level)
+
 app = Flask(__name__)
 app.register_blueprint(app_test)
 
 
 if __name__ == '__main__':
-    logging_config.config_logging(Log.log_name, Log.log_level)
     app.run(host='0.0.0.0', port=9002, debug=True)
