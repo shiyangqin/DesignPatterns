5d07839358f685bc4af4643afec6101b221a7da9
shiyangqin <shiyangqin@yeah.net>
2019-09-23 17:55:24 +0800

    添加do/test.py

---
+++ flask/do/test.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import logging
+
+from do.producer import Producer
+
+LOG = logging.getLogger(__name__)
+
+
+class Test(Producer):
+    def process(self, request):
+        res = {
+            "mseeage": "ok"
+        }
+        pg = self.get_pg()
+        redis = self.get_redis()
+        LOG.debug('this is Test')
+        return True, res
