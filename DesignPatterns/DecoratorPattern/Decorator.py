84640440cd44cdd9ab91e4392ebd2dc9ffcfcfe7
shiyangqin <shiyangqin@yeah.net>
2019-12-01 15:04:38 +0800

    更新

---
+++ DesignPatterns/DecoratorPattern/Decorator.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+装饰器
+"""
+import functools
+
+
+def log(func):
+    """无参数装饰器"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('call %s():' % func.__name__)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def log_with_param(text):
+    """有参数装饰器"""
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            print('call %s():' % func.__name__)
+            print('log_param = {}'.format(text))
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
